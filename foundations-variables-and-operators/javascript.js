68003822d9cddca4f763ceb6d176a9e3e3174420
Len <lenfullstack@proton.me>
2026-08-17 13:16:22 +0800

    Code along to foundations-variables-and-operators
    https://www.theodinproject.com/lessons/foundations-variables-and-operators

---
+++ foundations-variables-and-operators/javascript.js
@@ -0,0 +1,19 @@
+let firstName = "John";
+let lastName = "Doe";
+
+console.log(firstName);
+console.log(lastName);
+
+
+let age = 11;
+console.log(age); // outputs 11 to the console
+
+age = 54;
+
+console.log(age); // what will be output now?
+
+
+const pi = 3.14;
+pi = 10;
+
+console.log(pi); // What will be output?
